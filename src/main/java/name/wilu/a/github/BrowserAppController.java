2d995511381b848a72f8e366f150da2f237f31d5
Piotr <piotr.wilczek@wilu.name>
2017-08-16 23:59:04 +0200

    whaa?

--- src/main/java/name/wilu/a/github/BrowserAppController.java
+++ src/main/java/name/wilu/a/github/BrowserAppController.java
@@ -3,14 +3,14 @@ package name.wilu.a.github;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 @Controller
 public class BrowserAppController {
 
     @GetMapping("/repositories/{owner}/{repo}")
-    public ResponseEntity<?> userRepo(@RequestParam String owner, @RequestParam String repo) {
+    public ResponseEntity<?> repoDetails(@PathVariable String owner, @PathVariable String repo) {
         throw new NotImplementedException();
     }
 
