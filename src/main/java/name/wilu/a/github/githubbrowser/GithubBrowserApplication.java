ed6c7f563bc8fd41d71a2ba2b9016e3f5d7bb547
Piotr Wilczek <piotr.wilczek@bbh.com>
2017-08-16 09:20:51 +0200

    initial commit

---
+++ src/main/java/name/wilu/a/github/githubbrowser/GithubBrowserApplication.java
@@ -0,0 +1,12 @@
+package name.wilu.a.github.githubbrowser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubBrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubBrowserApplication.class, args);
+	}
+}
